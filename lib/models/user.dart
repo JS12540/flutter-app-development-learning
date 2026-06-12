7c0cd89303e2e8bec72061da82a6446de37e0c9b
Jay Shah <61103636+JS12540@users.noreply.github.com>
2026-06-12 11:45:02 +0530

    Create user.dart

---
+++ lib/models/user.dart
@@ -0,0 +1,31 @@
+/*
+=============================================================================
+
+MODEL
+
+Represents data.
+
+JSON
+ ↓
+User Object
+
+=============================================================================
+*/
+
+class User {
+  final int id;
+  final String name;
+
+  User({
+    required this.id,
+    required this.name,
+  });
+
+  factory User.fromJson(
+      Map<String, dynamic> json) {
+    return User(
+      id: json["id"],
+      name: json["name"],
+    );
+  }
+}
